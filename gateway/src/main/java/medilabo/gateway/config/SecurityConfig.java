0ea23f5f75bdb1fdf5e4fe729d9d09fb6c9fa94a
Gilles DESBONNETS <gillesdesbonnets@gmail.com>
2026-03-06 12:31:49 +0100

    [FEAT] ajout de la config de securité

--- gateway/src/main/java/medilabo/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/medilabo/gateway/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.medilabo.gateway.config;
+package medilabo.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
